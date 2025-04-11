d97fe066e2d53eecee153a677793e94d97daa7a7
Victor Thompson <thompsonvictor1206@gmail.com>
2025-04-11 23:40:39 +0100

    fix(UI):resolve layout shift due to side animation slide

--- src/components/VisionMission.tsx
+++ src/components/VisionMission.tsx
@@ -5,7 +5,7 @@ const VisionMission = () => {
   return (
     <div className="flex container flex-wrap gap-6 justify-center">
       <div>
-        <AnimateSections delay={0.5} type="slideInLeft">
+        <AnimateSections delay={0.8} type="popUp">
           <VisionMissionCard
             title="Our Vision"
             description="To be the trusted partner for businesses seeking to leverage technology to improve decision-making, unlock new opportunities, and achieve transformative growth"
@@ -14,7 +14,7 @@ const VisionMission = () => {
         </AnimateSections>
       </div>
       <div>
-        <AnimateSections delay={0.5} type="slideInRight">
+        <AnimateSections delay={0.8} type="popUp">
           <VisionMissionCard
             title="Our Mission"
             description="We bridge the gap between technology and business, delivering customized ICT solutions that drive efficiency, growth, and competitive advantage."
